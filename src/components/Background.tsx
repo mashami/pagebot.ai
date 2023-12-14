8712cc8297237718a79720dfa5b5df82b9b101a1
Maker Studios <mashamipaccy04@gmail.com>
2023-12-14 22:28:35 +0200

    fixed: changes on the background

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -5,7 +5,7 @@ import { BackgroundColor, StarsBackground } from "./SVGs/SVGs"
 const Background = () => {
   return (
     <div className="relative">
-      <span className="absolute w-screenoverflow-hidden">
+      <span className="absolute w-screen overflow-hidden">
         <BackgroundColor />
       </span>
       <span className="absolute top-16 w-screen overflow-hidden">
